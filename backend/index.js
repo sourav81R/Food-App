9146d4d73337f8773d7811740ac96ae13f36bace
Sourav Chowdhury <souravchowdhury0203@gmail.com>
2025-11-05 17:32:24 +0530

    fixing connetion issues

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://petpooja-food-app.vercel.app/",
     credentials:true
 }))
 app.use(express.json())
