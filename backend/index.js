d1a6825e9193106227e706c518cf45c77c35e15c
Sourav Chowdhury <souravchowdhury0203@gmail.com>
2025-11-06 22:39:07 +0530

    Add Correct URLs to Backend and Frontend

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,53 @@
-import express from "express"
-import dotenv from "dotenv"
-dotenv.config()
-import connectDb from "./config/db.js"
-import cookieParser from "cookie-parser"
-import authRouter from "./routes/auth.routes.js"
-import cors from "cors"
-import userRouter from "./routes/user.routes.js"
+import express from "express";
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import itemRouter from "./routes/item.routes.js"
-import shopRouter from "./routes/shop.routes.js"
-import orderRouter from "./routes/order.routes.js"
-import http from "http"
-import { Server } from "socket.io"
-import { socketHandler } from "./socket.js"
+import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import shopRouter from "./routes/shop.routes.js";
+import itemRouter from "./routes/item.routes.js";
+import orderRouter from "./routes/order.routes.js";
+import { socketHandler } from "./socket.js";
 
-const app=express()
-const server=http.createServer(app)
+dotenv.config();
 
-const io=new Server(server,{
-   cors:{
-    origin:"https://petpooja-food-app.vercel.app/",
-    credentials:true,
-    methods:['POST','GET']
-}
-})
-
-app.set("io",io)
+const app = express();
+const server = http.createServer(app);
 
 
+const io = new Server(server, {
+  cors: {
+    origin: "https://petpooja-food-app.vercel.app/",
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+});
 
-const port=process.env.PORT || 5000
-app.use(cors({
-    origin:"https://petpooja-food-app.vercel.app/",
-    credentials:true
-}))
-app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
-app.use("/api/shop",shopRouter)
-app.use("/api/item",itemRouter)
-app.use("/api/order",orderRouter)
+app.set("io", io);
 
-socketHandler(io)
-server.listen(port,()=>{
-    connectDb()
-    console.log(`server started at ${port}`)
-})
+app.use(
+  cors({
+    origin: "https://petpooja-food-app.vercel.app/",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/shop", shopRouter);
+app.use("/api/item", itemRouter);
+app.use("/api/order", orderRouter);
 
+socketHandler(io);
+
+
+const port = process.env.PORT || 5000;
+server.listen(port, async () => {
+  await connectDb();
+  console.log(` Server running on http://localhost:${port}`);
+});
